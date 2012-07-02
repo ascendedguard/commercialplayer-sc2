269aa9c4d59edc15222461873c98ab4268736863
Will Eddins <eddinsw@gmail.com>
2012-07-02 19:48:04 -0400

    Added error handling when the application crashes.

--- TwitchCommercialSC2/App.xaml.cs
+++ TwitchCommercialSC2/App.xaml.cs
@@ -9,10 +9,36 @@
 
 namespace TwitchCommercialSC2
 {
+    using System;
+    using System.Windows;
+
     /// <summary>
     /// Interaction logic for App.xaml
     /// </summary>
     public partial class App
     {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            AppDomain.CurrentDomain.UnhandledException += this.CurrentDomain_UnhandledException;
+
+            base.OnStartup(e);
+        }
+
+        void CurrentDomain_UnhandledException(object sender, UnhandledExceptionEventArgs e)
+        {
+            var exception = (Exception)e.ExceptionObject;
+            var error = exception.GetType().ToString() + " " + exception.Message;
+            while (exception.InnerException != null)
+            {
+                exception = exception.InnerException;
+                error += Environment.NewLine + exception.GetType().ToString() + " " + exception.Message;
+            }
+
+            MessageBox.Show(
+                "An error was thrown: " + Environment.NewLine + error,
+                "Error Thrown",
+                MessageBoxButton.OK,
+                MessageBoxImage.Error);
+        }
     }
 }
